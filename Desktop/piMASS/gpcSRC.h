7cf5bbb99a49faea58944b0a72021309c7694098
AliveMOon <alivemoongp@gmail.com>
2019-06-18 12:05:36 +0200

    class gpcSRC

---
+++ Desktop/piMASS/gpcSRC.h
@@ -0,0 +1,25 @@
+#ifndef GPCSRC_H
+#define GPCSRC_H
+
+
+class gpcSRC
+{
+public:
+    U1  *pA, *pB;
+    U4
+    gpcSRC();
+    virtual ~gpcSRC();
+
+protected:
+private:
+    gpcSRC(const gpcSRC& other){}
+    gpcSRC& operator=(const gpcSRC& b)
+    {
+        if( this == &b )
+            return *this; // handle self assignment
+        //assignment operator
+        return *this;
+    }
+};
+
+#endif // GPCSRC_H
