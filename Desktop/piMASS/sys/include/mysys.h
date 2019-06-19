fde648f712ee77d11a4f8fd09a3d4797e8616580
Dezső Bodor <alivemoon@infornax.hu>
2019-06-19 20:17:07 +0200

    add win define ->mysys.hu

--- Desktop/piMASS/sys/include/mysys.h
+++ Desktop/piMASS/sys/include/mysys.h
@@ -1,4 +1,9 @@
 // be kell másolni a user/include könyvtárba ahol keresni fogja a forrás
 // be lehet álítani, hogy mijen rendszerre fordítsuk le
-#define gpdSYSubi
-//#define gpdSYSpi
+#ifndef MYSYS_H_INCLUDED
+#define MYSYS_H_INCLUDED
+
+#define gpdSYSpi
+//#define gpdSYSubi
+//#define gpdSYSwin
+#endif // piMASS_H_INCLUDED
