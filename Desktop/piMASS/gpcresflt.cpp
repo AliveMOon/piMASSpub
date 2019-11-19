440f06f8c43c701887b3265f3bef2ae34d4c350e
AliveMOon <alivemoon@gmail.com>
2019-11-20 00:26:19 +0100

    gpcALU.typ() .AN() etc...bfx

--- Desktop/piMASS/gpcresflt.cpp
+++ Desktop/piMASS/gpcresflt.cpp
@@ -102,10 +102,15 @@ gpcALU& gpcALU::int2flt( gpcRES* pM, U4x2 xy, U1x4 ty4 ) {
 		pM->chg( *this );
 		return *this;
 	}
-	U4 nNEW = gpmPAD( nLD2+1, 0x10 );
+	/*U4 nNEW = gpmPAD( nLD2+1, 0x10 );
 	U1		*pS = (U1*)tmp.pDAT,
 			*pD = new U1[nNEW];
 	gpmZn( pD, nNEW );
+	pDAT = pD;*/
+	U1		*pS = (U1*)tmp.pDAT,
+			*pD = new U1[nALLOC()];
+	gpmZn( pD, AN().w );
+	pD[AN().w] = 0;
 	pDAT = pD;
 
 	if( !pS )
